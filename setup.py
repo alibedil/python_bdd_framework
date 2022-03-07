b5f31850928153460e9c342de3fff5f3ef875e69
Fayzali BEDILOV <58497125+alibedil@users.noreply.github.com>
2022-03-07 03:57:32 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+# from distutils.core import setup
+
+from setuptools import setup, find_packages
+
+
+setup(name = 'PythonBDD',
+      description = 'BDDPractice',
+      author = 'Fayzali BEDILOV',
+      author_email = 'fayzali.bedilov@gmail.com',
+      url = 'https://www.supersqa.com/',
+      packages = find_packages()
+      )
